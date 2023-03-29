8d9a48f3ab487217050bacbe0554bf5079ac97b6
Tim Newsome <tim@casualhacker.net>
2023-03-29 14:11:57 -0700

    Add ability to read staged git changes as input
    This commit adds a command line option to read staged git changes instead
    of diff from stdin. If the "--git" option is passed, the script will run
    "git diff --cached" and use its output as input for the GPT model.

--- commit-msg.py
+++ commit-msg.py
@@ -1,7 +1,9 @@
 #!/bin/env python3
 
+import argparse
 import openai
 import os
+import subprocess
 import sys
 
 # Set up the OpenAI API client
@@ -17,11 +19,32 @@ def gpt3_5(prompt):
 
     return response.choices[0]['message']['content']
 
-if __name__ == "__main__":
+def main():
+    parser = argparse.ArgumentParser(
+        description="""Use GPT to create source control commit messages.
+
+        Unless other arguments are passed, reads the diff from stdin.
+        """)
+    parser.add_argument("--git", "-g", help="Use staged git changes.",
+                        action="store_true")
+    args = parser.parse_args()
+
     prompt = """Write a git commit message for the following diff. The message
             must start with a one-line summary of 60 characters, then have a
             blank line, and then have a longer but concise description of the
             change."""
     prompt += "Here's the diff:"
-    prompt += sys.stdin.read()
+
+    if args.git:
+        diff = (
+            subprocess.check_output(['git', 'diff', '--cached']).decode('utf-8'))
+    else:
+        diff = sys.stdin.read()
+    if len(diff) < 5:
+        print("Empty diff.")
+        return 1
+    prompt += diff
     print(gpt3_5(prompt))
+
+if __name__ == "__main__":
+    sys.exit(main())
