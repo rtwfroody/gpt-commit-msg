b5136a50138349e91d7e2703c3416aadd40c8cfa
Tim Newsome <tim@casualhacker.net>
2023-03-30 09:48:41 -0700

    feat: Add ability to summarize long commit messages
    This commit adds functionality to the `commit-msg.py` script to summarize long commit messages using the OpenAI GPT-3 API. The script can now split long messages into smaller chunks and summarize each chunk individually, ensuring that the resulting summary fits within the maximum token count for the chosen GPT-3 model. The script also includes a function for counting tokens, and an option to choose the GPT-3 model to use.
    (gpt-4)

--- commit-msg.py
+++ commit-msg.py
@@ -6,6 +6,7 @@ import os
 import subprocess
 import sys
 import tiktoken
+import re
 
 max_token_count = {
     "gpt-4": 8192,
@@ -13,10 +14,14 @@ max_token_count = {
 }
 
 # Set up the OpenAI API client
-openai.api_key = os.environ["OPENAI_API_KEY"]
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 def gpt(prompt, model="gpt-3.5-turbo"):
     #print("prompt:", repr(prompt))
+
+    assert len(prompt) > 25
+    #response = f"{prompt[:300]!r}:{len(prompt)}"
+    #return response
 
     response = openai.ChatCompletion.create(
         model=model,
@@ -30,21 +35,84 @@ def gpt(prompt, model="gpt-3.5-turbo"):
     return response.choices[0]['message']['content']
 
 def token_count(text, model):
+    encoding = tiktoken.encoding_for_model(model)
     # tiktoken seems to be undercounting tokens compared to the API
-    return len(text)
-    #encoding = tiktoken.encoding_for_model(model)
-    #return len(encoding.encode(diff))
+    return len(encoding.encode(text)) * 2
 
-def summarize(text, model="gpt-3.5-turbo", prompt="Summarize the following:"):
+def commit_message(diff, model):
+    prompt = """Write a git commit message for the following. The message
+            starts with a one-line summary of 60 characters, then have a
+            blank line, and then have a longer but concise description of the
+            change."""
+
+    # Simple case. No summarizing needed.
+    tcount = token_count(prompt + diff, model)
+    if tcount <= max_token_count[model]:
+        return gpt(prompt + diff)
+
+    result = []
+    summaries = summarize(diff, model)
+    result.extend(summaries)
+    summary_string = "\n\n".join(summaries)
+    while True:
+        if token_count(prompt + summary_string, model) <= max_token_count[model]:
+            break
+        # Summarize the summary
+        summaries = summarize(summary_string, model)
+        result = summaries + ["## More Detail"] + result
+        summary_string = "\n\n".join(summaries)
+    
+    result.insert(0, gpt(prompt + summary_string, model))
+    return "\n\n".join(result)
+
+def summarize(text, model="gpt-3.5-turbo",
+              splitre=[
+                r"^(diff )", # First try to split by diff
+                "^$",        # Then try blank line
+                "\n",        # Then try newline
+                ]
+                ):
+    prompt = "Summarize the following:"
     query = prompt + text
     tcount = token_count(query, model)
 
-    if tcount > max_token_count[model]:
-        front = summarize(text[:len(text)//2])
-        back = summarize(text[len(text)//2:])
-        return front + "\n" + back
-    else:
-        return gpt(prompt + text)
+    if tcount <= max_token_count[model]:
+        return [gpt(prompt + text)]
+
+    summaries = []
+    parts = re.split(splitre[0], text, flags=re.MULTILINE)
+    combined_parts = []
+    # Now go back through and put the split string back together with the next
+    # thing
+    for part in parts:
+        if re.match(splitre[0], part) or not combined_parts:
+            combined_parts.append(part)
+        else:
+            combined_parts[-1] += part
+    parts = combined_parts
+
+    chunk = [parts[0]]
+    chunk_tcount = token_count(parts[0], model)
+    for part in parts[1:]:
+        part_tcount = token_count(part, model)
+        #print(f">>> {splitre[0]!r}",
+        #      chunk_tcount,
+        #      len(chunk),
+        #      part_tcount,
+        #      max_token_count[model])
+
+        if chunk_tcount + part_tcount >= max_token_count[model]:
+            text = "".join(chunk)
+            chunk = []
+            if token_count(text, model) > max_token_count[model]:
+                # Need to split using a different regex
+                summaries.extend(summarize(text, splitre=splitre[1:]))
+            else:
+                summaries.append(gpt(prompt + text))
+            chunk_tcount = sum(token_count(c, model) for c in chunk)
+        chunk.append(part)
+        chunk_tcount += part_tcount
+    return summaries
 
 def main():
     parser = argparse.ArgumentParser(
@@ -72,12 +140,7 @@ def main():
     else:
         model = "gpt-3.5-turbo"
 
-    prompt = """Write a git commit message for the following. The message
-            starts with a one-line summary of 60 characters, then have a
-            blank line, and then have a longer but concise description of the
-            change."""
-
-    print(summarize(diff, model, prompt))
+    print(commit_message(diff, model))
     print(f"({model})")
 
 if __name__ == "__main__":
