3b62a30034bc61bedeb1cbdfa1d485d774034ab5
Tim Newsome <tim@casualhacker.net>
2023-03-30 10:16:27 -0700

    Update commit-msg.py to use specified model for gpt, token_count
    This commit updates the commit-msg.py script to use the specified gpt model, passed through the command line arguments. The changes include:
    - Removing the hardcoded model from the gpt, token_count, and summarize functions.
    - Adding the 'args' global variable to reference command line arguments throughout the script.
    - Replacing 'model' variable with 'args.model' for proper functionality with specified model choice.
    (gpt-4)

--- commit-msg.py
+++ commit-msg.py
@@ -16,7 +16,7 @@ max_token_count = {
 # Set up the OpenAI API client
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-def gpt(prompt, model="gpt-3.5-turbo"):
+def gpt(prompt):
     #print("prompt:", repr(prompt))
 
     assert len(prompt) > 25
@@ -24,7 +24,7 @@ def gpt(prompt, model="gpt-3.5-turbo"):
     #return response
 
     response = openai.ChatCompletion.create(
-        model=model,
+        model=args.model,
         messages=[
             {"role": "user", "content": prompt}
         ]
@@ -34,38 +34,38 @@ def gpt(prompt, model="gpt-3.5-turbo"):
 
     return response.choices[0]['message']['content']
 
-def token_count(text, model):
-    encoding = tiktoken.encoding_for_model(model)
+def token_count(text):
+    encoding = tiktoken.encoding_for_model(args.model)
     # tiktoken seems to be undercounting tokens compared to the API
     return len(encoding.encode(text)) * 2
 
-def commit_message(diff, model):
+def commit_message(diff):
     prompt = """Write a git commit message for the following. The message
             starts with a one-line summary of 60 characters, then have a
             blank line, and then have a longer but concise description of the
             change."""
 
     # Simple case. No summarizing needed.
-    tcount = token_count(prompt + diff, model)
-    if tcount <= max_token_count[model]:
+    tcount = token_count(prompt + diff)
+    if tcount <= max_token_count[args.model]:
         return gpt(prompt + diff)
 
     result = []
-    summaries = summarize(diff, model)
+    summaries = summarize(diff)
     result.extend(summaries)
     summary_string = "\n\n".join(summaries)
     while True:
-        if token_count(prompt + summary_string, model) <= max_token_count[model]:
+        if token_count(prompt + summary_string) <= max_token_count[args.model]:
             break
         # Summarize the summary
-        summaries = summarize(summary_string, model)
+        summaries = summarize(summary_string)
         result = summaries + ["## More Detail"] + result
         summary_string = "\n\n".join(summaries)
     
-    result.insert(0, gpt(prompt + summary_string, model))
+    result.insert(0, gpt(prompt + summary_string))
     return "\n\n".join(result)
 
-def summarize(text, model="gpt-3.5-turbo",
+def summarize(text,
               splitre=[
                 r"^(diff )", # First try to split by diff
                 "^$",        # Then try blank line
@@ -74,9 +74,9 @@ def summarize(text, model="gpt-3.5-turbo",
                 ):
     prompt = "Summarize the following:"
     query = prompt + text
-    tcount = token_count(query, model)
+    tcount = token_count(query)
 
-    if tcount <= max_token_count[model]:
+    if tcount <= max_token_count[args.model]:
         return [gpt(prompt + text)]
 
     summaries = []
@@ -92,28 +92,29 @@ def summarize(text, model="gpt-3.5-turbo",
     parts = combined_parts
 
     chunk = [parts[0]]
-    chunk_tcount = token_count(parts[0], model)
+    chunk_tcount = token_count(parts[0])
     for part in parts[1:]:
-        part_tcount = token_count(part, model)
+        part_tcount = token_count(part)
         #print(f">>> {splitre[0]!r}",
         #      chunk_tcount,
         #      len(chunk),
         #      part_tcount,
-        #      max_token_count[model])
+        #      max_token_count[args.model])
 
-        if chunk_tcount + part_tcount >= max_token_count[model]:
+        if chunk_tcount + part_tcount >= max_token_count[args.model]:
             text = "".join(chunk)
             chunk = []
-            if token_count(text, model) > max_token_count[model]:
+            if token_count(text) > max_token_count[args.model]:
                 # Need to split using a different regex
                 summaries.extend(summarize(text, splitre=splitre[1:]))
             else:
                 summaries.append(gpt(prompt + text))
-            chunk_tcount = sum(token_count(c, model) for c in chunk)
+            chunk_tcount = sum(token_count(c) for c in chunk)
         chunk.append(part)
         chunk_tcount += part_tcount
     return summaries
 
+args = None
 def main():
     parser = argparse.ArgumentParser(
         description="""Use GPT to create source control commit messages.
@@ -124,6 +125,7 @@ def main():
                         action="store_true")
     parser.add_argument("--4", "-4", help="Use GPT4 (slower, costs more money)",
                         dest='gpt4', action="store_true")
+    global args
     args = parser.parse_args()
 
     if args.git:
@@ -136,12 +138,12 @@ def main():
         return 1
 
     if args.gpt4:
-        model = "gpt-4"
+        args.model = "gpt-4"
     else:
-        model = "gpt-3.5-turbo"
+        args.model = "gpt-3.5-turbo"
 
-    print(commit_message(diff, model))
-    print(f"({model})")
+    print(commit_message(diff))
+    print(f"({args.model})")
 
 if __name__ == "__main__":
     sys.exit(main())
